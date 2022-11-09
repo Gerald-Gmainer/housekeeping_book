723930130cb1b6445f70d933fe2f8c2ab4901fa5
Gerald Gmainer <gerald@thedesignium.com>
2022-11-09 23:43:37 +0900

    fix database scheme

--- database/setup.sql
+++ database/setup.sql
@@ -1,6 +1,12 @@
 CREATE TYPE category_type AS ENUM
   ('INCOME', 'OUTCOME');
 
+CREATE TABLE migrations (
+  id SERIAL PRIMARY KEY,
+  file_name text,
+  execution_date timestamp with time zone NOT NULL DEFAULT NOW()
+);
+
 CREATE TABLE category_icons (
   id SERIAL PRIMARY KEY,
   icon_path text NOT NULL,
@@ -16,13 +22,13 @@ CREATE TABLE account_icons (
 CREATE TABLE currencies (
   id SERIAL PRIMARY KEY,
   name text  NOT NULL,
-  precesion int default 2,
+  precesion int default 2
 );
 
 CREATE TABLE accounts (
   id SERIAL PRIMARY KEY,
   name text  NOT NULL,
-  icon references account_icons(id)  NOT NULL,
+  icon int references account_icons(id) NOT NULL,
   init_balance_amount numeric(12, 3) DEFAULT 0,
   init_balance_date timestamp  NOT NULL,
   include_in_balance boolean DEFAULT TRUE
@@ -31,21 +37,8 @@ CREATE TABLE accounts (
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY,
   name text  NOT NULL,
-  icon references category_icons(id)  NOT NULL,
+  icon int references category_icons(id)  NOT NULL,
   type category_type  NOT NULL
-);
-
-CREATE TABLE balances (
-  id SERIAL PRIMARY KEY,
-  year int NOT NULL,
-  month int NOT NULL,
-  amount numeric(12, 3) DEFAULT 0
-);
-
-CREATE TABLE balance_account_links (
-  id SERIAL PRIMARY KEY,
-  account_id accounts(id)  NOT NULL,
-  balance_id balances(id)  NOT NULL
 );
 
 CREATE TABLE bookings (
@@ -53,11 +46,29 @@ CREATE TABLE bookings (
   booking_date date NOT NULL,
   description text,
   amount numeric(12, 3) DEFAULT 0 NOT NULL,
-  category_id categories(id)  NOT NULL,
-  account_id accounts(id) NOT NULL,
-  currency_id currencies(id) NOT NULL,
-)
-
-CREATE TABLE settings (
-  id SERIAL PRIMARY KEY
+  category_id int references categories(id) NOT NULL,
+  account_id int references accounts(id) NOT NULL,
+  currency_id int references currencies(id) NOT NULL
 );
+
+
+
+
+
+
+-- CREATE TABLE balances (
+--   id SERIAL PRIMARY KEY,
+--   year int NOT NULL,
+--   month int NOT NULL,
+--   amount numeric(12, 3) DEFAULT 0
+-- );
+--
+-- CREATE TABLE balance_account_links (
+--   id SERIAL PRIMARY KEY,
+--   account_id accounts(id)  NOT NULL,
+--   balance_id balances(id)  NOT NULL
+-- );
+
+-- CREATE TABLE settings (
+--   id SERIAL PRIMARY KEY
+-- );
